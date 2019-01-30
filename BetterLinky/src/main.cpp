8375ebec2fea8a9a5df70e73350e826bd392a33a
Andy Pilate <cubox@cubox.me>
2019-01-30 23:01:18 +0100

    It works well now!

--- BetterLinky/src/main.cpp
+++ BetterLinky/src/main.cpp
@@ -1,90 +1,121 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
-#include <ESP8266mDNS.h>
-#include <WiFiUdp.h>
 #include <ArduinoOTA.h>
-#include <RemoteDebug.h>
 #include <TimeLib.h>
 #include <NTPClientLib.h>
 #include <Wire.h>
 #include <Adafruit_ADS1015.h>
-
+#include <WiFiClient.h>
 #include "secrets.h"
 
+#define DEBUGGING true
+
+#if DEBUGGING
+#include <RemoteDebug.h>
 RemoteDebug Debug;
+#endif
+
 Adafruit_ADS1115 ads;
 
+// Nicked this from somewhere, don't know don't ask
 const double multiplier = 0.0625;
+// This depends on the model of clamp being used
 const double FACTOR = 50;
 
+WiFiClient client;
+
 void setup() {
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(SSID, PASSWORD);
-  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
-    delay(5000);
-    ESP.restart();
-  }
+    Wire.begin(D4, D5); // Change if you wired things differently.
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(SSID, PASSWORD); // Hint: secrets.h
+    // Without WiFi being up, we are useless. Kill ourselves until it works.
+    while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+        delay(1000);
+        ESP.restart();
+    }
 
-  // Port defaults to 8266
-  // ArduinoOTA.setPort(8266);
+    ArduinoOTA.begin();
+    #if DEBUGGING
+    Debug.begin("linky.cubox");
+    #endif
+    NTP.begin("europe.pool.ntp.org", 1, true, 0);
+    // Since the max voltage the ADC will see is 1.42, we can double it
+    // To gain more precision. If you change this, maybe update hardcoded values?
+    ads.setGain(GAIN_TWO);
+    ads.begin();
+    client.setNoDelay(true);
+    // This is a local server running netcat -l -k -w 5 4200 | tee log
+    client.connect("192.168.0.252", 4200);
+}
 
-  // Hostname defaults to esp8266-[ChipID]
-  // ArduinoOTA.setHostname("myesp8266");
+// Defined later
+double getCurrent();
 
-  // No authentication by default
-  // ArduinoOTA.setPassword("admin");
+// A full loop should happen once per second, plus a liiiiittle bit each time
+// Might be more if we are trying to reconnect to the server, or if NTP needs 
+void loop() { 
+    double currentRMS;
+    double power;
+    time_t t; // Need to declare here of the goto won't work
 
-  // Password can be set with it's md5 value as well
-  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
-  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+    // Either we have no time, or it's wrong. This need to be updated in a year.
+    // Don't forget to update this!
+    // We are not reporting power use without the proper time
+    // But still need to call "end of loop" functions
+    // (Also will be more obvious if there is a problem, no output)
+    if (year() != 2019 && year() != 2020) { 
+        NTP.begin("europe.pool.ntp.org", 1, true, 0);
+        goto end; // Hehe, fight me
+    }
 
-  ArduinoOTA.onStart([]() {
-    String type;
-    if (ArduinoOTA.getCommand() == U_FLASH)
-      type = "sketch";
-    else // U_SPIFFS
-      type = "filesystem";
+    if (!client.connected()) {
+        #if DEBUGGING
+        rdebugAln("We are disconnected. Reconnecting");
+        #endif
+        client.connect("192.168.0.252", 4200);
+    }
+    t = now();
+    currentRMS = getCurrent();
+    power = 230 * currentRMS; // 230V, update if not using 230V
 
-    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-  });
-  ArduinoOTA.onEnd([]() {
-  });
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-  });
-  ArduinoOTA.onError([](ota_error_t error) {
+    #if DEBUGGING
+    rdebugA("%s: Current is: %fA, which makes power: %fW\n", NTP.getTimeDateString().c_str(), currentRMS, power);
+    #endif
     
-  });
-  ArduinoOTA.begin();
-  Debug.begin("Telnet_HostName");
-  NTP.begin("europe.pool.ntp.org", 1, true, 0);
-  ads.setGain(GAIN_TWO);
-  ads.begin();
+    if (client.connected()) {
+        // If you need a specific output format, this is where you set it up
+        client.printf("%ld %f %f\n", t, currentRMS, power);
+    // If despite the previous connect attempt, we are still not connected, well, fuck.
+    } else { 
+        #if DEBUGGING
+        rdebugAln("Despite trying to reconnect, no luck");
+        #endif
+    }
+
+end:
+    ArduinoOTA.handle();
+    #if DEBUGGING
+    Debug.handle();
+    #endif
 }
 
+// Got this from some website, not made by me
 double getCurrent() {
-  long t = millis();
-  long rawAdc = ads.readADC_Differential_0_1();
-  long minRaw = rawAdc;
-  long maxRaw = rawAdc;
-  while (millis() - t < 1000) {
-    rawAdc = ads.readADC_Differential_0_1();
-    maxRaw = maxRaw > rawAdc ? maxRaw : rawAdc;
-    minRaw = minRaw < rawAdc ? minRaw : rawAdc;
-  }
+    double voltage;
+    double current;
+    double sum = 0;
+    long t = millis();
+    int counter = 0;
 
-  maxRaw = maxRaw > minRaw ? maxRaw : -minRaw;
-  double voltagePeak = maxRaw * multiplier / 1000;
-  double voltageRMS = voltagePeak * 0.70710678118;
-  double currentRMS = voltageRMS * FACTOR;
-  return currentRMS;
-}
+    while (millis() - t < 1000) {
+        voltage = ads.readADC_Differential_0_1() * multiplier;
+        current = voltage * FACTOR;
+        current /= 1000.0;
 
-void loop() {
-  double currentRMS = getCurrent();
-  double power = 230 * currentRMS;
+        sum += sq(current);
+        counter++;
+    }
 
-  Debug.printf("Current is: %fA, which makes power: %fW\n", currentRMS, power);
-
-  ArduinoOTA.handle();
-  Debug.handle();
+    current = sqrt(sum / counter);
+    return(current);
 }
